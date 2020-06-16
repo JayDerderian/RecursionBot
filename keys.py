ac625dd00e1b583b59e246500d1a280be8660739
Jay Derderian <57573086+JayDerderian@users.noreply.github.com>
2020-06-15 18:59:48 -0700

    Delete keys.py

--- keys.py
+++
@@ -1,7 +0,0 @@
-keys = dict(
-    #Fill in as needed
-    CONSUMER_KEY =    'API Key',
-    CONSUMER_SECRET=  'API Secret Key',
-    ACCESS_KEY=       'Access Token',
-    ACCESS_SECRET=    'Access Token Secret',
-)
